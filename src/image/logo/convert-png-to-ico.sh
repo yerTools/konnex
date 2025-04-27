298d9c49f40b948099b6e8bbfaddeea9dca2e457
Felix Mayer <FelixM@yer.tools>
2025-04-28 00:19:45 +0200

    Add logo with text

--- src/image/logo/convert-png-to-ico.sh
+++ src/image/logo/convert-png-to-ico.sh
@@ -15,6 +15,17 @@ convert ./png/konnex-logo-16.png \
     ./png/konnex-logo-1024.png \
     ./ico/konnex-logo.ico
 
+convert ./png/konnex-logo-with-text-64.png \
+    ./png/konnex-logo-with-text-72.png \
+    ./png/konnex-logo-with-text-96.png \
+    ./png/konnex-logo-with-text-128.png \
+    ./png/konnex-logo-with-text-192.png \
+    ./png/konnex-logo-with-text-256.png \
+    ./png/konnex-logo-with-text-384.png \
+    ./png/konnex-logo-with-text-512.png \
+    ./png/konnex-logo-with-text-1024.png \
+    ./ico/konnex-logo-with-text.ico
+
 convert ./png/konnex-logo-16.png \
     ./png/konnex-logo-24.png \
     ./png/konnex-logo-32.png \
@@ -25,12 +36,21 @@ convert ./png/konnex-logo-16.png \
     ./png/konnex-logo-128.png \
     ./ico/konnex-logo-128.ico
 
+convert ./png/konnex-logo-with-text-64.png \
+    ./png/konnex-logo-with-text-72.png \
+    ./png/konnex-logo-with-text-96.png \
+    ./png/konnex-logo-with-text-128.png \
+    ./ico/konnex-logo-with-text-128.ico
+
 convert ./png/konnex-logo-16.png \
     ./png/konnex-logo-24.png \
     ./png/konnex-logo-32.png \
     ./png/konnex-logo-48.png \
     ./png/konnex-logo-64.png \
     ./ico/konnex-logo-64.ico
+
+convert ./png/konnex-logo-with-text-64.png \
+    ./ico/konnex-logo-with-text-64.ico
 
 convert ./png/konnex-logo-16.png \
     ./png/konnex-logo-24.png \
--- src/image/logo/convert-svg-to-png.sh
+++ src/image/logo/convert-svg-to-png.sh
@@ -91,55 +91,66 @@ for size in 48 57 58 60; do
 done
 
 for size in 64 70 72 76 80 96 100 120 128 144 150 152; do
-    flatpak run org.inkscape.Inkscape \
-        ./svg/konnex-logo.svg \
-        --export-width=$size --export-height=$size \
-        --export-type=png \
-        --export-filename="./png/konnex-logo-$size-big.png"
+    for name in konnex-logo konnex-logo-with-text; do
+        
+        flatpak run org.inkscape.Inkscape \
+            ./svg/$name.svg \
+            --export-width=$size --export-height=$size \
+            --export-type=png \
+            --export-filename="./png/$name-$size-big.png"
 
-    zopflipng -m \
-        --lossy_transparent \
-        --lossy_8bit \
-        --iterations=64 \
-        --filters=01234mepb \
-        ./png/konnex-logo-$size-big.png \
-        ./png/konnex-logo-$size.png
+        zopflipng -m \
+            --lossy_transparent \
+            --lossy_8bit \
+            --iterations=64 \
+            --filters=01234mepb \
+            ./png/$name-$size-big.png \
+            ./png/$name-$size.png
 
-    rm ./png/konnex-logo-$size-big.png
+        rm ./png/$name-$size-big.png
+    
+    done
 done
 
 for size in 167 180 192 256 310 384 420 512; do
-    flatpak run org.inkscape.Inkscape \
-        ./svg/konnex-logo.svg \
-        --export-width=$size --export-height=$size \
-        --export-type=png \
-        --export-filename="./png/konnex-logo-$size-big.png"
+    for name in konnex-logo konnex-logo-with-text; do
 
-    zopflipng -m \
-        --lossy_transparent \
-        --lossy_8bit \
-        --iterations=32 \
-        --filters=01234mepb \
-        ./png/konnex-logo-$size-big.png \
-        ./png/konnex-logo-$size.png
+        flatpak run org.inkscape.Inkscape \
+            ./svg/$name.svg \
+            --export-width=$size --export-height=$size \
+            --export-type=png \
+            --export-filename="./png/$name-$size-big.png"
 
-    rm ./png/konnex-logo-$size-big.png
+        zopflipng -m \
+            --lossy_transparent \
+            --lossy_8bit \
+            --iterations=32 \
+            --filters=01234mepb \
+            ./png/$name-$size-big.png \
+            ./png/$name-$size.png
+
+        rm ./png/$name-$size-big.png
+
+    done
 done
 
 for size in 558 768 1024; do
-    flatpak run org.inkscape.Inkscape \
-        ./svg/konnex-logo.svg \
-        --export-width=$size --export-height=$size \
-        --export-type=png \
-        --export-filename="./png/konnex-logo-$size-big.png"
+    for name in konnex-logo konnex-logo-with-text; do
+        flatpak run org.inkscape.Inkscape \
+            ./svg/$name.svg \
+            --export-width=$size --export-height=$size \
+            --export-type=png \
+            --export-filename="./png/$name-$size-big.png"
 
-    zopflipng -m \
-        --lossy_transparent \
-        --lossy_8bit \
-        --iterations=16 \
-        --filters=01234mepb \
-        ./png/konnex-logo-$size-big.png \
-        ./png/konnex-logo-$size.png
+        zopflipng -m \
+            --lossy_transparent \
+            --lossy_8bit \
+            --iterations=16 \
+            --filters=01234mepb \
+            ./png/$name-$size-big.png \
+            ./png/$name-$size.png
 
-    rm ./png/konnex-logo-$size-big.png
+        rm ./png/$name-$size-big.png
+
+    done
 done
